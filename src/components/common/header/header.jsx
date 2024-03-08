eb6ebc3d8b8f697316aefa3e8aa9aebadb91c9d8
Ali ihsan Ertugrul <ertugrulali3538@gmail.com>
2024-03-08 23:43:02 +0300

    bugs fixed

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -4,24 +4,33 @@ import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
+import "./style.scss";
 
-const Header = async({lang}) => {
-    const session = await getSession();
-    const dict=await getDictionary(lang)
+const Header = async ({ lang }) => {
+  const session = await getSession();
+  const dict = await getDictionary(lang);
   return (
     <nav className="navbar navbar-expand-lg bg-primary sticky-top ">
       <div className="container">
-        <Link className="navbar-brand d-none d-md-block text-success rounded" href="/">
-          <Image src="/teklifimgelsin.png" width={160} height={40} alt="teklifim-gelsin-logo"/>
+        <Link className="navbar-brand  text-success rounded" href="/">
+          <Image
+            src="/teklifimgelsin.png"
+            width={160}
+            height={40}
+            alt="teklifim-gelsin-logo"
+          />
         </Link>
-        {session?.user?.name && <div className="nav-link active  text-success p-2 fw-bold " aria-current="page" href="#">
-              {dict.products.welcome} {session?.user?.name} 👋
-              </div>}
+        {session?.user?.name && (
+          <div
+            className="nav-link active  text-success p-2 fw-bold "
+            aria-current="page"
+            href="#"
+          >
+            {dict.products.welcome} {session?.user?.name} 👋
+          </div>
+        )}
         <div>
           <ul className="nav">
-            
-            
-
             <li className="nav-item dropdown ">
               <a
                 className="nav-link dropdown-toggle text-success text-capitalize"
@@ -36,25 +45,29 @@ const Header = async({lang}) => {
                 {i18n.locales.map((locale, index) => (
                   <li key={index}>
                     <Link className="dropdown-item" href={`/${locale}`}>
-                     <span className={`fi fi-${locale==="en"? "gb":locale}`}></span> {locale.toLocaleUpperCase()}
+                      <span
+                        className={`fi fi-${locale === "en" ? "gb" : locale}`}
+                      ></span>{" "}
+                      {locale.toLocaleUpperCase()}
                     </Link>
                   </li>
                 ))}
               </ul>
             </li>
             <li className="nav-item">
-            {session?.user?.name && (
-            <form
-              action={async () => {
-                "use server";
-                await logout();
-                redirect(`${lang}`);
-              }}
-            >
-              <button className="btn btn-success " type="submit">{dict.products.logout}</button>
-            </form>
-          )}
-              
+              {session?.user?.name && (
+                <form
+                  action={async () => {
+                    "use server";
+                    await logout();
+                    redirect(`${lang}`);
+                  }}
+                >
+                  <button className="btn btn-success " type="submit">
+                    {dict.products.logout}
+                  </button>
+                </form>
+              )}
             </li>
           </ul>
         </div>
